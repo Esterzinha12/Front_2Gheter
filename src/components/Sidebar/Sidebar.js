115602b2cd7c15f022862716f301b7d7e011a616
Ester Girelli <ester_girelli@estudante.sesisenai.org.br>
2024-10-29 21:00:15 -0300

    sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faBars, faMagnifyingGlass, faSquarePlus, faFileSignature, faMessage, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import './Sidebar.css';
 
 export function Sidebar({ isHome }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className="barra-lateral">
+    <div className={`barra-lateral ${isExpanded ? 'expanded' : ''}`}>
       <ul>
-        <li><a href="#"><FontAwesomeIcon icon={faBars} size="xl" className="icone" /></a></li>
+        <li>
+          <a href="#" onClick={toggleSidebar}>
+            <FontAwesomeIcon icon={faBars} size="xl" className="icone" />
+          </a>
+        </li>
         <li>
           <a href="#" className={isHome ? 'active' : ''}>
             <FontAwesomeIcon icon={faHouse} size="xl" className={`icone ${isHome ? 'hover' : ''}`} />
+            {isExpanded && <span className="tab-name">Home</span>}
           </a>
         </li>
-        <li><a href="#"><FontAwesomeIcon icon={faMagnifyingGlass} size="xl" className="icone" /></a></li>
-        <li><a href="#"><FontAwesomeIcon icon={faSquarePlus} size="xl" className="icone" /></a></li>
-        <li><a href="#"><FontAwesomeIcon icon={faFileSignature} size="xl" className="icone" /></a></li>
-        <li><a href="#"><FontAwesomeIcon icon={faMessage} size="xl" className="icone" /></a></li>
+        <li>
+          <a href="#">
+            <FontAwesomeIcon icon={faSquarePlus} size="xl" className="icone" />
+            {isExpanded && <span className="tab-name">Meus anúncios</span>}
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <FontAwesomeIcon icon={faFileSignature} size="xl" className="icone" />
+            {isExpanded && <span className="tab-name">Contratos</span>}
+          </a>
+        </li>
       </ul>
       <div className="logout-container">
-        <a href="#"><FontAwesomeIcon icon={faRightFromBracket} size="xl" className="icone icone-logout" /></a>
+        <a href="#">
+          <FontAwesomeIcon icon={faRightFromBracket} size="xl" className="icone" />
+          {isExpanded && <span className="tab-name">Sair</span>}
+        </a>
       </div>
     </div>
   );
