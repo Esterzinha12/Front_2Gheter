4b5ea6a9d6d08099e9b163f94e13b05592ec2c2d
Ester Girelli <ester_girelli@estudante.sesisenai.org.br>
2025-03-18 21:12:29 -0300

    18/03

--- src/pages/MeusAnuncios/MeusAnuncios.js
+++ src/pages/MeusAnuncios/MeusAnuncios.js
@@ -1,54 +1,42 @@
 import React, { useState } from "react";
 import { Navbar } from "../../components/Navbar";
 import { Sidebar } from "../../components/Sidebar";
-import Card from "../../components/Card/Card"; 
-import CadastrarAnuncioModal from "../../components/CadastrarAnuncioModal/CadastrarAnuncioModal"; 
-import imagemExemplo from "../../components/Card/imagem.jpg"; 
-import './MeusAnuncios.css'; 
+import Card from "../../components/Card/Card";
+import CadastrarAnuncioModal from "../../components/CadastrarAnuncioModal/CadastrarAnuncioModal";
+import imagemExemplo from "../../components/Card/imagem.jpg";
+import "./MeusAnuncios.css";
 
 export function MeusAnuncios() {
   const [modalVisivel, setModalVisivel] = useState(false);
 
-  const abrirModal = (event) => {
-    event.preventDefault(); 
+  const abrirModal = () => {
+    console.log("Abrindo modal..."); // Debug
     setModalVisivel(true);
   };
 
   const fecharModal = () => {
+    console.log("Fechando modal..."); // Debug
     setModalVisivel(false);
   };
 
   return (
     <div className="body">
-      <Navbar /> 
-      
-      <div className="layout"> 
-        <Sidebar isHome={false} /> 
-        
-        <div className="conteudo-principal"> 
+      <Navbar />
+      <div className="layout">
+        <Sidebar isHome={false} />
+        <div className="conteudo-principal">
           <div className="header">
             <h1>Meus anúncios</h1>
-            <button 
-              className="anunciar-button"
-              onClick={abrirModal}
-              type="button" 
-            >
+            <button className="anunciar-button" onClick={abrirModal} type="button">
               Anunciar
             </button>
           </div>
-          
-          <div className="lista-cards"> 
-            <Card
-              imagem={imagemExemplo}
-              titulo="Título do Anúncio 1"
-              descricao="Descrição breve do anúncio 1."
-              className="card-anuncio"
-            />
+          <div className="lista-cards">
+            <Card imagem={imagemExemplo} titulo="Título do Anúncio 1" descricao="Descrição breve do anúncio 1." className="card-anuncio" />
           </div>
         </div>
       </div>
-
-      {modalVisivel && <CadastrarAnuncioModal visible={modalVisivel} onClose={fecharModal} />}
+      <CadastrarAnuncioModal visible={modalVisivel} onClose={fecharModal} />
     </div>
   );
 }
